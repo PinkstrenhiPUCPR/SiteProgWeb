24a507746e31c0d7673378a5146e700ff97fb25f
Pinkstrenhi <90482483+PinkstrenhiPUCPR@users.noreply.github.com>
2021-11-17 23:38:14 -0300

    Add files via upload

---
+++ ProjetoSite/js/Cart.js
@@ -0,0 +1,29 @@
+storage = window.localStorage;
+var shopList = JSON.parse(storage.getItem("shopList"))||"[]";
+
+window.onload = function(){
+    buildProductCart();
+}
+function buildProductCart(){
+    document.getElementById("cart").innerHTML = "";
+
+        for(var i = 0; i < shopList.length; i++)
+        {        
+            var content = "";
+            content += '<div class="CartTxt">';
+            content += '<table cellpadding="15" cellspacing="15">';
+            content += '<tr>';
+            content += '<td>';
+            content += '<img src="images/'+ shopList[i][2] + '">';
+            content += '</td>';
+            content += '<td>'+ shopList[i][1] +'</td>';
+            content += '<td>Preço: ' + shopList[i][3] + '</td>';
+            content += '</tr>';
+            content += '</table>';
+            content += '</div>';
+            document.getElementById("cart").innerHTML += content;
+        }
+}
+function buyCart(){
+    storage.removeItem("shopList");
+}
---
+++ ProjetoSite/js/Initial.js
@@ -0,0 +1,32 @@
+var productsList = 
+[
+    [0, "Pulseira Pena", "BaseItem.png", false],
+    [1, "Pulseira Noite", "nightImg.png", false],
+    [2, "Pulseira Arco-Iris", "rainbowImg.png", false],
+
+    
+];
+var shopList = [];
+window.onload = function(){
+    buildProductCard();
+}
+function buildProductCard(){
+    document.getElementById("SiteProductsCards").innerHTML = "";
+
+    for(var i = 0; i < productsList.length; i++)
+    {        
+        var content = "";
+        /* Card Pulseira Pena*/ 
+        content += '<div class="card">';
+        content += '<div class="productImg">';
+        content += '<img src="images/'+ productsList[i][2] + '">';
+        content += '</div>';
+        content += '<div class="productDescription">';
+        content +=  productsList[i][1];
+        content += '</div>';
+        content += '</div>';
+        /* */
+        document.getElementById("SiteProductsCards").innerHTML += content;
+    }
+}
+
